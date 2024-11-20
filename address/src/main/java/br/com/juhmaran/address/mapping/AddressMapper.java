6d749d46fb586b6cf145c1333b0490a7273227fa
JulianeMaran32 <julianemaran@gmail.com>
2024-11-20 10:23:03 -0300

    feat: adiciona mapeamento usando mapstruct

--- address/src/main/java/br/com/juhmaran/address/mapping/AddressMapper.java
+++ address/src/main/java/br/com/juhmaran/address/mapping/AddressMapper.java
@@ -1,4 +1,33 @@
-package br.com.juhmaran.address.services;
+package br.com.juhmaran.address.mapping;
 
-public interface AddressService {
+import br.com.juhmaran.address.domain.dtos.request.AddressRequest;
+import br.com.juhmaran.address.domain.dtos.response.AddressResponse;
+import br.com.juhmaran.address.domain.entities.AddressEntity;
+import br.com.juhmaran.address.domain.model.ViaCepResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AddressMapper {
+
+    AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    AddressEntity addressRequestToEntity(AddressRequest addressRequest);
+
+    AddressResponse addressEntityToResponse(AddressEntity addressEntity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "country", ignore = true)
+    @Mapping(target = "zipCode", source = "cep")
+    @Mapping(target = "street", source = "logradouro")
+    @Mapping(target = "complement", source = "complemento")
+    @Mapping(target = "unit", source = "unidade")
+    @Mapping(target = "neighborhood", source = "bairro")
+    @Mapping(target = "city", source = "localidade")
+    @Mapping(target = "stateAbbreviation", source = "uf")
+    @Mapping(target = "state", source = "estado")
+    AddressEntity viaCepResponseToEntity(ViaCepResponse viaCepResponse);
+
 }
